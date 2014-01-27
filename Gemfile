727f51efe2c1ae5e2820d4aba83a74f03187a1ee
Piotr Solnica <piotr.solnica@gmail.com>
2014-01-28 00:32:35 +0100

    Add test dummy rails app

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in rom-rails.gemspec
 gemspec
+
+group :test do
+  gem 'rspec-core', '~> 2.14'
+  gem 'rspec-expectations', '~> 2.14'
+end
---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '1f964276d8745979a6c19e6e675c59db2c43e32eba0c1455c980d78140e3da2e31ade807a416abee4b17532a8ec0af8d3dc5d46ae04b2077a882e1da79aeae4a'
