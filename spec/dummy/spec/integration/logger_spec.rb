2a3dcd66ff535ad02738760e179a2d5240b95619
Piotr Solnica <piotr.solnica@gmail.com>
2014-11-28 22:07:21 +0100

    Skip setting up logger since rom-sql supports instrumentation now \o/
    closes #3

--- spec/dummy/spec/integration/logger_spec.rb
+++ spec/dummy/spec/integration/logger_spec.rb
@@ -4,6 +4,8 @@ describe 'ROM logger' do
   let(:rom) { Rails.application.config.rom.env }
 
   it 'sets up rails logger for all repositories' do
+    pending 'this will be re-enabled once we have feature detection on adapters ' +
+      'and in case of missing rails-log-subscriber support we will set logger to Rails.logger'
     rom.repositories.each_value do |repository|
       expect(repository.logger).to be(Rails.logger)
     end
