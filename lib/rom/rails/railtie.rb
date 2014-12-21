e85014e7402b66c0194de1229b26191e4bc5b29e
Piotr Solnica <piotr.solnica@gmail.com>
2014-12-21 12:21:27 +0100

    Disconnect repos on to_prepare

--- lib/rom/rails/railtie.rb
+++ lib/rom/rails/railtie.rb
@@ -5,16 +5,20 @@ require 'rom/rails/configuration'
 require 'rom/rails/controller_extension'
 
 if defined?(Spring)
-  Spring.after_fork do
-    ROM.env.repositories.each_value do |repository|
-      repository.adapter.disconnect
-    end
-  end
+  Spring.after_fork { ROM::Rails::Railtie.disconnect }
 end
 
 module ROM
   module Rails
     class Railtie < ::Rails::Railtie
+      def self.disconnect
+        return unless ROM.env
+
+        ROM.env.repositories.each_value do |repository|
+          repository.adapter.disconnect
+        end
+      end
+
       def self.load_all
         %w(relations mappers commands).each do |type|
           load_files(type, ::Rails.root)
@@ -41,6 +45,7 @@ module ROM
 
       initializer "rom:prepare" do |app|
         config.to_prepare do |_config|
+          Railtie.disconnect
           app.config.rom.setup!
           app.config.rom.load!
           app.config.rom.finalize!
