ef414840dc47b928356d19f1d8dc77b733e49e3d
Alexander Flatter <flatter@fastmail.fm>
2015-01-07 11:45:18 +0100

    Configuration exposes repositories explicitely
    This commit changes the configuration API to be more friendly to the user and
    allows to configure repositories in an initializer like this:
    
        Rails.application.configure do |app|
          app.config.rom.repos[:yaml] = {uri: 'yaml:///config'}
        end

--- lib/rom/rails/configuration.rb
+++ lib/rom/rails/configuration.rb
@@ -1,17 +1,36 @@
 module ROM
   module Rails
     class Configuration
-      attr_reader :config
+      attr_reader :repos
 
+      # Tries to guess the right ROM configuration for a Rails app.
+      #
+      # @param [Rails::Application] app
+      # @return [Configuration]
+      #
+      # @api private
       def self.build(app)
-        config = app.config.database_configuration[::Rails.env].
-          symbolize_keys.update(root: app.config.root)
+        new(repos: derive_repos_from_application(app))
+      end
 
-        new(ROM::Config.build(config))
+      # Uses database configuration from Rails to configure repositories.
+      #
+      # Note that the `DATABASE_URL` environment variable supported by
+      # ActiveRecord will *NOT* be respected.
+      #
+      # @param [Rails::Application] app
+      # @return [Hash]
+      #
+      # @api private
+      def self.derive_repos_from_application(app)
+        config = app.config.database_configuration[::Rails.env].
+                            symbolize_keys.update(root: app.config.root)
+
+        ROM::Config.build(config)
       end
 
       def initialize(config)
-        @config = config
+        @repos = config.fetch(:repos)
       end
     end
   end
--- lib/rom/rails/railtie.rb
+++ lib/rom/rails/railtie.rb
@@ -52,7 +52,7 @@ module ROM
       private
 
       def self.setup!
-        ROM.setup(config.rom.config.symbolize_keys)
+        ROM.setup(config.rom.repos.symbolize_keys)
 
         load_all
 
