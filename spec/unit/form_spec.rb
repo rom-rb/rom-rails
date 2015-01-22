87776c11c81e2a959fb8f093a95467185b88121b
Piotr Solnica <piotr.solnica@gmail.com>
2015-01-22 12:26:38 +0100

    Refactor form spec

--- spec/unit/form_spec.rb
+++ spec/unit/form_spec.rb
@@ -1,32 +1,27 @@
 require 'spec_helper'
 
 describe 'Form' do
-  describe 'input and validator DSL' do
-    it 'defines params handler and validator' do
-      form = Class.new(ROM::Model::Form) do
-        def self.name
-          'UserForm'
-        end
-
-        input do
-          set_model_name 'User'
-          attribute :email, String
-        end
-
-        validations do
-          validates :email, presence: true
-        end
+  subject(:form) do
+    Class.new(ROM::Model::Form) do
+      def self.name
+        'UserForm'
       end
 
+      input do
+        set_model_name 'User'
+        attribute :email, String
+      end
+
+      validations do
+        validates :email, presence: true
+      end
+    end
+  end
+
+  describe 'input DSL' do
+    it 'defines params handler' do
       expect(form.params.attribute_set[:email]).not_to be(nil)
       expect(form.params.model_name).to eql('User')
-      expect(form.validator).not_to be(nil)
-
-      expect { form.validator.call(email: 'jane@doe') }.not_to raise_error
-
-      expect { form.validator.call(email: '') }.to raise_error(
-        ROM::Model::ValidationError
-      )
     end
 
     it 'raises error when attribute is in conflict with form interface' do
@@ -37,6 +32,18 @@ describe 'Form' do
           end
         end
       }.to raise_error(ArgumentError, /commit! attribute is in conflict/)
+    end
+  end
+
+  describe 'validator DSL' do
+    it 'defines validator' do
+      expect(form.validator).not_to be(nil)
+
+      expect { form.validator.call(email: 'jane@doe') }.not_to raise_error
+
+      expect { form.validator.call(email: '') }.to raise_error(
+        ROM::Model::ValidationError
+      )
     end
   end
 end
