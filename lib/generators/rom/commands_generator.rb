337ba2e5e84e7769f48f65a848cc4892a24a1f35
Chris Flipse <cflipse@gmail.com>
2015-02-12 12:36:14 -0500

    dry up the command generator

--- lib/generators/rom/commands_generator.rb
+++ lib/generators/rom/commands_generator.rb
@@ -3,22 +3,26 @@ require 'generators/rom'
 module ROM
   module Generators
     class CommandsGenerator < Base
-      def create_commands_file
-        template(
-          'create.rb.erb',
-          File.join('app', 'commands', file_name, 'create.rb')
-        )
 
-        template(
-          'update.rb.erb',
-          File.join('app', 'commands', file_name, 'update.rb')
-        )
 
-        template(
-          'delete.rb.erb',
-          File.join('app', 'commands', file_name, 'delete.rb')
-        )
+      def create_create_command
+        template 'create.rb.erb', command_file(:create)
       end
+
+      def create_update_command
+        template 'update.rb.erb', command_file(:update)
+      end
+
+      def create_delete_command
+        template 'delete.rb.erb', command_file(:delete)
+      end
+
+    private
+
+      def command_file(command)
+        File.join('app', 'commands', file_name, "#{command}.rb")
+      end
+
     end
   end
 end
