daddb241709f9417d32ca739ed821f1037d028f6
Piotr Solnica <piotr.solnica@gmail.com>
2015-03-16 12:52:10 +0100

    Add yard docs for validator extension

--- lib/rom/rails/model/validator.rb
+++ lib/rom/rails/model/validator.rb
@@ -24,6 +24,9 @@ module ROM
     #
     # @api public
     module Validator
+      # Inclusion hook that extends a class with required interfaces
+      #
+      # @api private
       def self.included(base)
         base.class_eval do
           extend ClassMethods
@@ -32,17 +35,32 @@ module ROM
         end
       end
 
+      # @return [Model::Attributes]
+      #
+      # @api private
       attr_reader :attributes
+
       delegate :model_name, to: :attributes
 
+      # @api private
       def initialize(attributes)
         @attributes = attributes
       end
 
+      # @return [Model::Attributes]
+      #
+      # @api public
       def to_model
         attributes
       end
 
+      # Trigger validations and return attributes on success
+      #
+      # @raises ValidationError
+      #
+      # @return [Model::Attributes]
+      #
+      # @api public
       def call
         raise ValidationError, errors unless valid?
         attributes
@@ -50,20 +68,49 @@ module ROM
 
       private
 
+      # This is needed for ActiveModel::Validations to work properly
+      # as it expects the object to provide attribute values. Meh.
+      #
+      # @api private
       def method_missing(name)
         attributes[name]
       end
 
       module ClassMethods
+        # Set relation name for a validator
+        #
+        # This is needed for validators that require database access
+        #
+        # @example
+        #
+        #   class UserValidator
+        #     include ROM::Model::Validator
+        #
+        #     relation :users
+        #
+        #     validates :name, uniqueness: true
+        #   end
+        #
+        # @return [Symbol]
+        #
+        # @api public
         def relation(name = nil)
           @relation = name if name
           @relation
         end
 
+        # FIXME: this looks like not needed
         def model_name
           attributes.model_name
         end
 
+        # Trigger validation for specific attributes
+        #
+        # @param [Model::Attributes] attributes The attributes for validation
+        #
+        # @raises [ValidationError]
+        #
+        # @return [Model::Attributes]
         def call(attributes)
           validator = new(attributes)
           validator.call
