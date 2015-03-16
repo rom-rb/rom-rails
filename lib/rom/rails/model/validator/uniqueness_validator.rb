f8f648a8d212c0ca96994faab0a5c5e04695a23a
Piotr Solnica <piotr.solnica@gmail.com>
2015-03-16 13:02:57 +0100

    Add yard docs for uniqueness validator

--- lib/rom/rails/model/validator/uniqueness_validator.rb
+++ lib/rom/rails/model/validator/uniqueness_validator.rb
@@ -3,33 +3,69 @@ require 'active_model/validator'
 module ROM
   module Model
     module Validator
+      # Uniqueness validation
+      #
+      # @api public
       class UniquenessValidator < ActiveModel::EachValidator
-        attr_reader :klass, :message
+        # Relation validator class
+        #
+        # @api private
+        attr_reader :klass
 
+        # error message
+        #
+        # @return [String, Symbol]
+        #
+        # @api private
+        attr_reader :message
+
+        # @api private
         def initialize(options)
           super
           @klass = options.fetch(:class)
           @message = options.fetch(:message) { :taken }
         end
 
+        # Hook called by ActiveModel internally
+        #
+        # @api private
         def validate_each(validator, name, value)
           validator.errors.add(name, message) unless unique?(name, value)
         end
 
         private
 
+        # Get relation object from the rom env
+        #
+        # @api private
         def relation
           rom.relations[relation_name]
         end
 
+        # Relation name defined on the validator class
+        #
+        # @api private
         def relation_name
           klass.relation
         end
 
+        # Shortcut to access global rom env
+        #
+        # @return [ROM::Env]
+        #
+        # @api private
         def rom
           ROM.env
         end
 
+        # Ask relation if a given attribute value is unique
+        #
+        # This uses `Relation#unique?` interface that not all adapters can
+        # implement.
+        #
+        # @return [TrueClass,FalseClass]
+        #
+        # @api private
         def unique?(name, value)
           relation.unique?(name => value)
         end
