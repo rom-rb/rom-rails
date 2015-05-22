63893830ffaaf5a90fddcf967b4f86e8acc1e4d2
Piotr Solnica <piotr.solnica@gmail.com>
2015-05-22 13:54:30 +0200

    As always...fix setup for jruby

--- spec/dummy/config/initializers/rom.rb
+++ spec/dummy/config/initializers/rom.rb
@@ -1,4 +1,5 @@
 ROM::Rails::Railtie.configure do |config|
-  config.repositories[:default] = [:sql, "sqlite://#{Rails.root}/db/#{Rails.env}.sqlite3"]
+  scheme = RUBY_ENGINE == 'jruby' ? 'jdbc:sqlite' : 'sqlite'
+  config.repositories[:default] = [:sql, "#{scheme}://#{Rails.root}/db/#{Rails.env}.sqlite3"]
   config.repositories[:test] = [:test_adapter, foo: :bar]
 end
