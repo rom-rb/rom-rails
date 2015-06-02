5a84695012c12c630e4fc1d87d185f313167bfb8
Piotr Solnica <piotr.solnica@gmail.com>
2015-06-02 10:41:23 +0200

    Deprecate repositories in configuration

--- lib/rom/rails/configuration.rb
+++ lib/rom/rails/configuration.rb
@@ -1,11 +1,16 @@
 require 'virtus'
+require 'rom/support/deprecations'
 
 module ROM
   module Rails
     class Configuration
+      extend ROM::Deprecations
+
       include Virtus.model(strict: true)
 
       attribute :gateways, Hash, default: {}
+
+      deprecate :repositories, :gateways
     end
   end
 end
