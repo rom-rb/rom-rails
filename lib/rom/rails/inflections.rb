759d5eaa9a22889c84b14b7e4b47c40c26e46b68
Chris Flipse <cflipse@gmail.com>
2015-06-10 11:46:18 -0400

    skip ROM inflection if Acronym method not known

--- lib/rom/rails/inflections.rb
+++ lib/rom/rails/inflections.rb
@@ -1,5 +1,6 @@
 require 'active_support/inflections'
 
 ActiveSupport::Inflector.inflections do |inflect|
-  inflect.acronym 'ROM'
+  # `acronym` was added in ActiveSupport 3.2.5
+  inflect.acronym 'ROM' if inflect.respond_to?(:acronym)
 end
