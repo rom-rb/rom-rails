31575a2430bf9cb5a4e0cdcae9490467e4db789c
Piotr Solnica <piotr.solnica@gmail.com>
2015-06-15 23:42:32 +0200

    Add LazyEnv#mappers; docs and some isolated specs

--- Rakefile
+++ Rakefile
@@ -1,10 +1,17 @@
 require 'bundler/setup'
 require 'rubocop/rake_task'
 
-task default: %w(app:spec rubocop)
+task default: %w(app:spec spec:isolated rubocop)
 
 APP_RAKEFILE = File.expand_path("../spec/dummy/Rakefile", __FILE__)
 load 'rails/tasks/engine.rake'
+
+desc 'Run isolated specs'
+task :"spec:isolated" do
+  FileList["spec/isolation/*_spec.rb"].each do |spec|
+    sh "rspec", spec
+  end
+end
 
 RuboCop::RakeTask.new do |task|
   task.options << '--display-cop-names'
--- lib/rom-rails.rb
+++ lib/rom-rails.rb
@@ -6,6 +6,15 @@ require 'rom/rails/railtie'
 require 'rom/model'
 
 module ROM
+  # Lazy env is set to defer loading whole env during rails booting process
+  #
+  # This prevents situations where components are defined but db doesn't exist
+  # (this can often happen in development)
+  #
+  # Accessing any of the components using this env will trigger finalization
+  # and final env will be set on ROM.env
+  #
+  # @api private
   class LazyEnv
     attr_reader :rom
 
@@ -28,6 +37,11 @@ module ROM
       env.command(*args, &block)
     end
 
+    def mappers
+      finalize
+      env.mappers
+    end
+
     def finalize
       rom.finalize
     end
@@ -37,15 +51,21 @@ module ROM
     end
   end
 
+  # Overridden finalize sets up lazy-env first which eventually results in the
+  # finalized env on the first access of any components.
+  #
+  # @api private
   def self.finalize
     if env.is_a?(LazyEnv)
       begin
         @env = @boot.finalize
+        self
       ensure
         @boot = nil
       end
     else
       @env = LazyEnv.new(self)
+      self
     end
   end
 end
