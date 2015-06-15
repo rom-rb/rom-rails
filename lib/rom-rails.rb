934e74afd429a83e46b5fd1fdc8124e3f9e045c7
Piotr Solnica <piotr.solnica@gmail.com>
2015-06-15 22:45:54 +0200

    Lazy-finalize the env

--- lib/rom-rails.rb
+++ lib/rom-rails.rb
@@ -4,3 +4,48 @@ require 'rom'
 require 'rom/rails/version'
 require 'rom/rails/railtie'
 require 'rom/model'
+
+module ROM
+  class LazyEnv
+    attr_reader :rom
+
+    def initialize(rom)
+      @rom = rom
+    end
+
+    def gateways
+      finalize
+      env.gateways
+    end
+
+    def relation(*args, &block)
+      finalize
+      env.relation(*args, &block)
+    end
+
+    def command(*args, &block)
+      finalize
+      env.command(*args, &block)
+    end
+
+    def finalize
+      rom.finalize
+    end
+
+    def env
+      rom.env
+    end
+  end
+
+  def self.finalize
+    if env.is_a?(LazyEnv)
+      begin
+        @env = @boot.finalize
+      ensure
+        @boot = nil
+      end
+    else
+      @env = LazyEnv.new(self)
+    end
+  end
+end
