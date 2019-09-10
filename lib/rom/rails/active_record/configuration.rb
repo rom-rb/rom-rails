2f343a249407809268ed730d7daf4e3f33503ec3
Chris Flipse <cflipse@gmail.com>
2019-09-10 10:28:36 -0400

    separate a uri builder from the AR config reader

--- lib/rom/rails/active_record/configuration.rb
+++ lib/rom/rails/active_record/configuration.rb
@@ -1,4 +1,4 @@
-require 'addressable/uri'
+require_relative 'uri_builder'
 
 module ROM
   module Rails
@@ -43,64 +43,12 @@ module ROM
           uri_options = config.except(:adapter).merge(scheme: adapter)
           other_options = config.except(*BASE_OPTIONS)
 
-          builder_method = :"#{adapter}_uri"
-          uri = if respond_to?(builder_method)
-                  send(builder_method, uri_options)
-                else
-                  generic_uri(uri_options)
-                end
+          builder = ROM::Rails::ActiveRecord::UriBuilder.new
 
-          # JRuby connection strings require special care.
-          if RUBY_ENGINE == 'jruby' && adapter != 'postgresql'
-            uri = "jdbc:#{uri}"
-          end
-
+          uri = builder.build(adapter, uri_options)
           { uri: uri, options: other_options }
         end
 
-        def self.sqlite3_uri(config)
-          path = Pathname.new(config.fetch(:root)).join(config.fetch(:database))
-
-          build_uri(
-            scheme: 'sqlite',
-            host: '',
-            path: path.to_s
-          )
-        end
-
-        def self.postgresql_uri(config)
-          generic_uri(config.merge(
-                        host: config.fetch(:host) { '' },
-                        scheme: 'postgres'
-                      ))
-        end
-
-        def self.mysql_uri(config)
-          if config.key?(:username) && !config.key?(:password)
-            config.update(password: '')
-          end
-
-          generic_uri(config)
-        end
-
-        def self.generic_uri(config)
-          build_uri(
-            scheme: config.fetch(:scheme),
-            user: escape_option(config[:username]),
-            password: escape_option(config[:password]),
-            host: config[:host],
-            port: config[:port],
-            path: config[:database]
-          )
-        end
-
-        def self.build_uri(attrs)
-          Addressable::URI.new(attrs).to_s
-        end
-
-        def self.escape_option(option)
-          option.nil? ? option : CGI.escape(option)
-        end
       end
     end
   end
